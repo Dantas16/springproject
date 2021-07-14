747b3aec6ccca2152f96336c033157d0c9a89448
J0aoCosta <jmsfc.costa@gmail.com>
2021-07-14 16:07:44 +0100

    fixes, added buyer create, list, all

--- src/main/java/com/training/springbootbuyitem/entity/request/CreateBuyerRequestDto.java
+++ src/main/java/com/training/springbootbuyitem/entity/request/CreateBuyerRequestDto.java
@@ -5,9 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateUserRequestDto {
+
+    @NotEmpty
+    private String name;
 }
